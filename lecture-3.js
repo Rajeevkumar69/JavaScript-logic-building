d7c034c67cf840422d3df52d27f5749168c3e21b
Rajeevkumar69 <rk488296@gmail.com>
2026-01-10 08:30:04 +0530

    lecture-3

--- lecture-3.js
+++ lecture-3.js
@@ -3,12 +3,55 @@ function reverseString(str) {
           return 'Enter valid string';
      }
 
+     let result = '';
 
-     return str.split('').reverse().join();
+     for (let i = str.length - 1; i >= 0; i--) {
+          result += str[i]
+     }
+     return result;
 
-     // let result = '';
-
-     // for(let i = 0; i < str.length; i++){
-     //      result = 
-     // }
+     // return str.split('').reverse().join('');
 }
+
+console.log(reverseString('Software development'));
+
+
+function findDuplicate(arrayArg) {
+     if (!Array.isArray(arrayArg)) {
+          return 'Enter valid array';
+     }
+
+     let seen = new Set();
+     let result = [];
+
+     for (let i = 0; i < arrayArg.length; i++) {
+          if (seen.has(arrayArg[i])) {
+               result.push(arrayArg[i]);
+          } else {
+               seen.add(arrayArg[i]);
+          }
+     }
+
+     return result;
+}
+
+console.log(findDuplicate([1, 2, 5, 5, 1, 5, 4, 2, 3]));
+
+
+function findLargest(arr) {
+     if (!Array.isArray(arr)) {
+          return 'Enter valid array';
+     }
+
+     let result = arr[0];
+
+     for (let i = 0; i < arr.length; i++) {
+          if (arr[i] > result) {
+               result = arr[i];
+          }
+     }
+
+     return result;
+}
+
+console.log(findLargest([1, 2, 5, 5, 1, 5, 4, 2, 3]));
