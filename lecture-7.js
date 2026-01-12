2511ca5c40c59814cec10ea959ac8ecfd7dd497f
Rajeevkumar69 <rk488296@gmail.com>
2026-01-12 11:47:34 +0530

    lecture-7

--- lecture-7.js
+++ lecture-7.js
@@ -11,4 +11,53 @@ function printTable(tableNumber, end) {
      }
 }
 
-printTable(2,10);
+printTable(2, 10);
+
+let arrData = ['are you Okay', 'i am good'];
+
+function getAllVowelsAsArray(arr) {
+     if (!Array.isArray(arr)) {
+          return `Enter valid array only`
+     }
+
+     if (arr.every(element => typeof element !== 'string')) {
+          return 'Enter string array only!';
+     }
+
+     const vowels = 'aeiouAEIOU';
+     let result = [];
+
+     for (let str of arr) {
+          for (let char of str) {
+               if (vowels.includes(char)) {
+                    result.push(char);
+               }
+          }
+     }
+
+     return result;
+}
+
+console.log(getAllVowelsAsArray(arrData));
+
+let strData = 'hElloWorld!';
+
+function getAllVowelsAsString(strInput) {
+     if (typeof strInput !== 'string') {
+          return `Enter valid string only!`;
+     }
+
+     const vowels = 'aeiouAEIOU';
+
+     let result = '';
+
+     for (let i = 0; i < strInput.length; i++) {
+          if (vowels.includes(strInput[i])) {
+               result += strInput[i];
+          }
+     }
+
+     return result;
+}
+
+console.log(getAllVowelsAsString(strData));
